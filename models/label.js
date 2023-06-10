4abbb2b1c216f6b9388dfcf732dbe89a0d8e3c85
Kareem Ellis <kareemellis55@gmail.com>
2023-06-09 21:46:33 -0500

    Created models for label and note

---
+++ models/label.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoode')
+
+const labelSchema = new mongoose.Schema({
+  name: String
+})
+
+labelSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Label', labelSchema)
---
+++ models/note.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+  title: String,
+  details: String,
+  pinned: Boolean,
+  labels: Array
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
